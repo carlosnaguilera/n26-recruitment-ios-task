c0cb5f62ee140b097131978d335ed3faf1588bd3
Carlos <carlosnaguilera@gmail.com>
2025-05-17 14:35:19 +0200

    🐛 use NavigationView instead of NavigationStack

--- Modules/Features/Assets/Sources/Assets/List/AssetsList.swift
+++ Modules/Features/Assets/Sources/Assets/List/AssetsList.swift
@@ -20,13 +20,10 @@ public struct AssetsList: View {
     }
     
     public var body: some View {
-        NavigationStack {
+        NavigationView {
             StatefulView(state: viewModel.state,
                          retryAction: { Task { try? await viewModel.onAppear() } },
                          content: assetsList)
-            .navigationDestination(for: AssetListItem.State.self) { selection in
-                AssetsFactory.makeAssetDetailsView(assetId: selection.id)
-            }
             .listStyle(.plain)
             .navigationTitle(Strings.title)
             .toolbar {
@@ -40,7 +37,7 @@ public struct AssetsList: View {
     
     private func assetsList(_ state: [AssetListItem.State]) -> some View {
         List(state) { itemState in
-            NavigationLink(value: itemState) {
+            NavigationLink(destination: AssetsFactory.makeAssetDetailsView(assetId: itemState.id)) {
                 AssetListItem(state: itemState)
             }
         }
